7d87747c815c82fc17ccf7cba41c4328c1e488f3
TuomasMoilanen <35306402+TuomasMoilanen@users.noreply.github.com>
2018-03-06 16:12:23 +0200

    more testing...

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ function assembleDeck(){
     shuffle();
   }
   function shuffle(){
-    for (let i = 0; i < 100000; i++) {
+    for (let i = 0; i < 10000; i++) {
       let loc1 = Math.floor((Math.random() * deck.length));
       let loc2 = Math.floor((Math.random() * deck.length));
       let temp = deck[loc1];
